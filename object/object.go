8e12530301f980cc6b7fe0fb7cbec05f97c00fcf
miguelhun <hunmiguel1124@gmail.com>
2020-10-01 16:28:42 -0500

    builtin function 'len'

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ import (
 )
 
 type ObjectType string
+type BuiltinFunction func(args ...Object) Object
 
 const (
 	INTEGER_OBJ      = "INTEGER"
@@ -18,6 +19,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -90,3 +92,10 @@ func (f *Function) Inspect() string {
 	return out.String()
 
 }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
